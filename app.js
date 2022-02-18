1458eaa6c9b9a6e6ddb9a303472627b87f21563e
KH Rasedul Islam <87899184+khrasedul-dev@users.noreply.github.com>
2022-02-18 23:11:14 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,79 @@
+const fs = require('fs')
+const {Telegraf , Composer} = require('micro-bot')
+
+const dbModel = require('./model')
+const postId = "620fcca24e419ac556a20278"
+
+const bot = new Composer()
+
+
+
+//DB mail gejaho3293@rubygon.com pass: rps
+mongoose.connect('mongodb+srv://rasedul20:rasedul20@telegramproject.6rm9z.mongodb.net/telegramDB?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true}).catch((e)=>{
+        console.log(e)
+}).then((d)=>console.log('Database connected')).catch((e)=>console.log(e))
+
+
+
+
+bot.start((ctx)=>{
+    ctx.reply("This is private groups bot").catch('Something is wrong')
+})
+
+bot.on('new_chat_members',ctx=>{
+    ctx.reply(`Hello ${ctx.from.first_name} \n❗Welcome to Vape Lovers❗  \n🌀🌀TRUSTED SELLER🌀🌀  \n■■■■■AUTHENTIC ■■■■■`)
+})
+
+
+bot.hears('vlbotstart',ctx=>{
+
+    const findQuery = {
+        id : postId
+    }
+    dbModel.find(findQuery, (e,data)=>{
+        if (e) {
+            console.log(e)
+        } else {
+
+            ctx.telegram.sendMessage(ctx.chat.id , data[0].message ).catch('Something is wrong')
+
+            setInterval(()=>{
+                ctx.telegram.sendMessage(ctx.chat.id , data[0].message ).catch('Something is wrong')
+            },1800000)
+
+        }
+    })
+
+})
+
+
+
+
+bot.hears(/setvlbotmessage/gi,(ctx)=>{
+    const text = ctx.update.message.text
+    const finaltext = text.replace(/setvlbotmessage/gi,"")
+    const textForSaved = finaltext.trim()
+
+    const updateQuery = {
+        id: postId
+    }
+
+    const inputData = {
+        message: textForSaved
+    }
+
+    dbModel.updateOne(updateQuery,inputData,(e)=>{
+        if (e) {
+            console.log(e)
+        } else {
+            ctx.reply("Your message sucessfully set").catch("Something is wrong")
+        }
+    })
+})
+
+
+
+
+
+
+module.exports = bot
